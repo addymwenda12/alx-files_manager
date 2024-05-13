2e83fc417e8048a8c2e220390eaa5c5f1a0ba8c5
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-13 11:05:43 +0300

    Refactored RedisClient class to use async with promisify

--- utils/redis.js
+++ utils/redis.js
@@ -5,28 +5,25 @@ class RedisClient {
   constructor() {
     this.client = redis.createClient();
     this.getAsync  = promisify(this.client.get).bind(this.client);
-    this.setAsync = promisify(this.client.set).bind(this.client);
-    this.delAsync = promisify(this.client.del).bind(this.client);
     this.client.on('error', (error) => {
       console.error(`Redis client not connected to the server: ${error.message}`);
     });
   }
 
   isAlive() {
-  return this.client.connected;
+    return this.client.connected;
   }
 
   async get(key) {
-    return await this.getAsync(key);
+    return this.getAsync(key);
   }
 
   async set(key, value, duration) {
-    await this.setAsync(key, value);
-    this.client.expire(key, duration);
+    this.client.setex(key, duration, value);
   }
 
   async del(key) {
-    await this.delAsync(key);
+    this.client.del(key);
   }
 }
 
