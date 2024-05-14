54edcda4928dd398d63cf9f67be36f90d46ff2d6
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-14 12:02:49 +0300

    Fixed unhandles promise rejection in DBClient class

--- utils/db.js
+++ utils/db.js
@@ -1,30 +1,47 @@
-const { MongoClient } = require('mongodb');
+import { MongoClient } from 'mongodb';
 
 class DBClient {
   constructor() {
-		const host = process.env.DB_HOST || 'localhost';
-		const port = process.env.DB_PORT || 27017;
-		const database = process.env.DB_DATABASE || 'files_manager';
-		const url = `mongodb://${host}:${port}/${database}`;
+    this.host = process.env.DB_HOST || 'localhost';
+		this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
 
-		this.client = new MongoClient(url, { useUnifiedTopology: true });
-		this.client.connect();
+    this.uri = `mongodb://${this.host}:${this.port}/${this.database}`;
+    this.client = new MongoClient(this.uri, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+	async connect() {
+		try {
+			await this.client.connect();
+		} catch (err) {
+			console.error(err);
+			process.exit(1);
+		}
 	}
 
-	isAlive() {
-		return this.client.isConnected();
-	}
+  isAlive() {
+    return this.client.topology.isConnected();
+  }
 
-	async nbUsers() {
-		const usersCollection = this.client.db().collection('users');
-		return usersCollection.countDocuments({});
-	}
+  async nbUsers() {
+    try {
+      const users = await this.client.db(this.database).collection('users').countDocuments();
+      return users;
+    } catch (err) {
+      throw new Error(`Unable to get number of users ${err.message}`);
+    }
+  }
 
-	async nbFiles() {
-		const filesCollection = this.client.db().collection('files');
-		return filesCollection.countDocuments({});
-	}
+  async nbFiles() {
+    try {
+      const files = await this.client.db(this.database).collection('files').countDocuments();
+      return files;
+    } catch (err) {
+      throw new Error(`Unable to get number of files ${err.message}`);
+    }
+  }
 }
 
 const dbClient = new DBClient();
-module.exports = dbClient;
+export default dbClient;
