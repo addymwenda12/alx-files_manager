a06dcccb7ed6ebf3b85a979a4acab1b58fcf193e
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-15 12:31:03 +0300

    Add new routes for user authentication and profile retieval

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const AppController = require('../controllers/AppController');
+const AuthController = require('../controllers/AuthController');
+const UserController = require('../controllers/UserController');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UserController.getMe);
 
 module.exports = router;
