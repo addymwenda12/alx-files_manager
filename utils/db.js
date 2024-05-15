9347770e1a2afdd6ec43c0bfb2e5bffeaed9b99a
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-15 20:52:51 +0300

    Installed necessary dependencies

--- utils/db.js
+++ utils/db.js
@@ -2,44 +2,39 @@ import { MongoClient } from 'mongodb';
 
 class DBClient {
   constructor() {
-    this.host = process.env.DB_HOST || 'localhost';
-		this.port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
-
-    this.uri = `mongodb://${this.host}:${this.port}/${this.database}`;
-    this.client = new MongoClient(this.uri, { useUnifiedTopology: true });
-    this.client.connect();
+    const HOST = process.env.DB_HOST || 'localhost';
+    const PORT = process.env.BD_PORT || 27017;
+    const DATABASE = process.env.DB_DATABASE || 'files_manager';
+    const URI = `mongodb://${HOST}:${PORT}`;
+    this.mongoClient = new MongoClient(URI, { useUnifiedTopology: true });
+    this.mongoClient.connect((error) => {
+      if (!error) this.db = this.mongoClient.db(DATABASE);
+    });
   }
 
-	async connect() {
-		try {
-			await this.client.connect();
-		} catch (err) {
-			console.error(err);
-			process.exit(1);
-		}
-	}
-
   isAlive() {
-    return this.client.topology.isConnected();
+    return this.mongoClient.isConnected();
+  }
+
+  getCollection(collectionName) {
+    const collection = this.db.collection(collectionName);
+    return collection;
   }
 
   async nbUsers() {
-    try {
-      const users = await this.client.db(this.database).collection('users').countDocuments();
-      return users;
-    } catch (err) {
-      throw new Error(`Unable to get number of users ${err.message}`);
-    }
+    const usersCollection = this.getCollection('users');
+    const numberOfUsers = await usersCollection.countDocuments();
+    return numberOfUsers;
   }
 
   async nbFiles() {
-    try {
-      const files = await this.client.db(this.database).collection('files').countDocuments();
-      return files;
-    } catch (err) {
-      throw new Error(`Unable to get number of files ${err.message}`);
-    }
+    const filesCollection = this.getCollection('files');
+    const numberOfFiles = filesCollection.countDocuments();
+    return numberOfFiles;
+  }
+
+  async close() {
+    await this.mongoClient.close();
   }
 }
 
