fab9548dfbbe437073d1616c369e3b35181d9336
lelenguyasameria <somearia711@gmail.com>
2024-05-16 12:03:53 +0300

    correction of task number 4

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,17 +2,25 @@ const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
 
 class AppController {
-	static async getStatus(req, res) {
-		const redisAlive = redisClient.isAlive();
-		const dbAlive = dbClient.isAlive();
-		await res.status(200).json({ redis: redisAlive, db: dbAlive });
+	static getStatus(request, response) {
+	  try {
+		const redis = redisClient.isAlive();
+		const db = dbClient.isAlive();
+		response.status(200).send({ redis, db });
+	  } catch (error) {
+		console.log(error);
+	  }
 	}
-
-	static async getStats(req, res) {
+  
+	static async getStats(request, response) {
+	  try {
 		const users = await dbClient.nbUsers();
 		const files = await dbClient.nbFiles();
-		res.status(200).json({ users, files });
+		response.status(200).send({ users, files });
+	  } catch (error) {
+		console.log(error);
+	  }
 	}
-}
-
-export default AppController;
+  }
+  
+  export default AppController;
